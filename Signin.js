1e061eeb329a9d61321795f78d7f6d09d0b1c1ab
purvipatel183 <100862029+purvipatel183@users.noreply.github.com>
2023-06-09 15:36:12 +0530

    Add files via upload

--- Signin.js
+++ Signin.js
@@ -1,29 +1,35 @@
-import React from 'react'
-import { Form, Row,Col } from 'react-bootstrap'
+import React from "react";
+import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
+
 const Signin = () => {
   return (
     <>
-    <Form>
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
-        <Form.Label column sm="2">
-          Email
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control plaintext readOnly defaultValue="email@example.com" />
-        </Col>
-      </Form.Group>
+   <div className="card shadow-sm border-0 px-3 rounded-2 mb-3 py-4 mx-auto mt-5 bg-light" style={{width:"500px"}}>
+    <div className="card-header bg-transparent border-0 text-center text-uppercase"><h3>Welcome to Exam_material.edu</h3></div>
+    <div className="card-body">
+        
+      <form action="/"  encType="multipart/form-data" autoComplete="off">
+        <div className="form-group">
+          <label className="mb-0">Enrollment no<span className="text-danger">*</span></label>
+          <input name="enrollment" type="number" className="form-control" placeholder="Enrollment no" />
+          
+        </div>
+        <div className="form-group">
+          <label className="mb-0">Password<span className="text-danger">*</span></label>
+          <input name="password" type="password" className="form-control" placeholder="Password" />
+          
+        </div>
+        <p className="text-center mb-0 mt-4"><Link type="submit" to="/home" className="btn btn-primary btn-lg w-100 text-uppercase" >Login</Link></p>
+        <h3 style={{textAlign:'center'}}>or</h3>
+        <p className="text-center mb-0 mt-4"><Link type="submit" to="/Signup" className="btn btn-primary btn-lg w-100 text-uppercase" >Sign up</Link></p>
 
-      <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
-        <Form.Label column sm="2">
-          Password
-        </Form.Label>
-        <Col sm="10">
-          <Form.Control type="password" placeholder="Password" />
-        </Col>
-      </Form.Group>
-    </Form>
+      </form>
+      
+    </div>
+  </div>
     </>
-  )
-}
+  );
+};
 
-export default Signin
+export default Signin;
---
+++ Signup.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+const Signup = () => {
+  return (
+    <>
+
+    <div className="card shadow-sm border-0 px-3 rounded-2 mb-3 py-4 mx-auto mt-5 bg-light" style={{width:"500px"}}>
+    <div className="card-header bg-transparent border-0 text-center text-uppercase"><h3>Welcome to Exam_material.edu</h3></div>
+    <div className="card-body">
+        
+      <form action="/"  encType="multipart/form-data" autoComplete="off">
+        <div className="form-group">
+          <label className="mb-0">Enrollment no<span className="text-danger">*</span></label>
+          <input name="enrollment" type="number" className="form-control" placeholder="Enrollment no" />
+          
+        </div>
+        <div className="form-group">
+          <label className="mb-0"> Create Password<span className="text-danger">*</span></label>
+          <input name="password" type="password" className="form-control" placeholder="Password" />
+        </div>
+        <p className="text-center mb-0 mt-4"><Link type="submit" to="/Home" className="btn btn-primary btn-lg w-100 text-uppercase" >Sign up</Link></p>
+
+
+      </form>
+      
+    </div>
+  </div>
+    </>
+  )
+}
+
+export default Signup
